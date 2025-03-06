59ce1f50ae421e667f4197da4bf170f6cd8ad5cd
Varirus <varirusek@gmail.com>
2025-03-06 16:02:22 +0100

    FizzBuzz split file

---
+++ FizzBuzz.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab0
+{
+    class FizzBuzz
+    {
+        int upperRange = 100;
+        int num = 0;
+
+        public FizzBuzz(int number)
+        {
+            if (number > upperRange)
+                num = upperRange;
+            else
+                num = number;
+        }
+        public void show()
+        {
+            if (num % 3 == 0)
+                Console.Write("Fizz");
+            if (num % 5 == 0)
+                Console.Write("Buzz");
+            if (!(num % 3 == 0 || num % 5 == 0))
+                Console.Write(num);
+            Console.Write("\n");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,30 +1,5 @@
 ﻿namespace Lab0
 {
-    class FizzBuzz
-    {
-        int upperRange = 100;
-        int num = 0;
-
-        public FizzBuzz(int number)
-        {
-            if (number > upperRange)
-                num = upperRange;
-            else
-                num = number;
-        }
-        public void show()
-        {
-            if (num % 3 == 0)
-                Console.Write("Fizz");
-            if (num % 5 == 0)
-                Console.Write("Buzz");
-            if (!(num % 3 == 0 || num % 5 == 0))
-                Console.Write(num);
-            Console.Write("\n");
-        }
-    }
-
-
     internal class Program
     {
         static void Main(string[] args)
