46c3ddbd9b77be8fa17b02efc9687b472c234337
Varirus <varirusek@gmail.com>
2025-03-06 16:07:45 +0100

    fixed

--- FizzBuzz.cs
+++ FizzBuzz.cs
@@ -8,25 +8,25 @@ namespace Lab0
 {
     class FizzBuzz
     {
-        int upperRange = 100;
-        int num = 0;
+        int upperRange = 0;
 
         public FizzBuzz(int number)
         {
-            if (number > upperRange)
-                num = upperRange;
-            else
-                num = number;
+            upperRange = number;
         }
         public void show()
         {
-            if (num % 3 == 0)
-                Console.Write("Fizz");
-            if (num % 5 == 0)
-                Console.Write("Buzz");
-            if (!(num % 3 == 0 || num % 5 == 0))
-                Console.Write(num);
-            Console.Write("\n");
+            for (int num = 1; num <= upperRange; num++)
+            {
+                if (num % 3 == 0)
+                    Console.Write("Fizz");
+                if (num % 5 == 0)
+                    Console.Write("Buzz");
+                if (!(num % 3 == 0 || num % 5 == 0))
+                    Console.Write(num);
+                Console.Write("\n");
+            }
+            
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -4,15 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            FizzBuzz fb1 = new FizzBuzz(45);
+            FizzBuzz fb1 = new FizzBuzz(20);
             fb1.show();
-            FizzBuzz fb2 = new FizzBuzz(20);
-            fb2.show();
-            FizzBuzz fb3 = new FizzBuzz(22);
-            fb3.show();
-            FizzBuzz fb4 = new FizzBuzz(21);
-            fb4.show();
         }
     }
 }
